5a9548d0ddf15b4af62c68824472f8d2edf0a7f6
rachellomas12 <125256700+rachellomas12@users.noreply.github.com>
2023-03-11 15:18:52 +1000

    Simple Dice Game

--- Simple_Dice_Game/script.js
+++ Simple_Dice_Game/script.js
@@ -21,26 +21,33 @@ let result;
 
 // on button click execute dice rolling function for each type of dice in dice arrays
 button.onclick = function () {
-  for (let i = 0; i < diceStr.length; i++) {
-    result = dice.roll(diceNum[i]);
-    printNumber(result, diceStr[i]);
-  }
-};
+  //Get the value from dropdown after click roll dice button
+  let diceDropDownValue = document.getElementById("sidesDropdown").value;
+  console.log('The dropdown value is ${diceDropDownValue}')
+  
+  let firstDice = document.getElementById("d4");
+  let secondDice = document.getElementById("d6");
 
-function dropDownChanged(_id){
-  let diceCol = document.querySelector(`#${_id}`);
-  let diceTitle = diceCol.querySelector("#container");
-  let diceDropDown = diceCol.querySelector("#sidesDropdown");
-  let diceCustomInput = diceCol.querySelector("#customSides");
+  let randomNumberBasedOnDropdown1 = Math.floor(Math.random() * diceDropDownValue) + 1;
+  let randomNumberBasedOnDropdown2 = Math.floor(Math.random() * diceDropDownValue) + 1;
 
-  if (diceDropDown.value == "Custom"){
-      diceTitle.innerHTML = `D ${diceCustomInput.value}`;
-      diceCustomInput.style.display = "inline";
-  }
-  else{
-      diceTitle.innerHTML = `D ${diceDropDown.value}`;
-      diceCustomInput.style.display = "none";
-  }
+  firstDice.innerHTML = randomNumberBasedOnDropdown1;
+  secondDice.innerHTML = randomNumberBasedOnDropdown2;
+
+//function dropDownChanged(_id){
+//  let diceCol = document.querySelector(`#${_id}`);
+//  let diceTitle = diceCol.querySelector("#container");
+//  let diceDropDown = diceCol.querySelector("#sidesDropdown");
+//  let diceCustomInput = diceCol.querySelector("#customSides");
+
+//  if (diceDropDown.value == "Custom"){
+//      diceTitle.innerHTML = `D ${diceCustomInput.value}`;
+//      diceCustomInput.style.display = "inline";
+//  }
+//  else{
+//      diceTitle.innerHTML = `D ${diceDropDown.value}`;
+//      diceCustomInput.style.display = "none";
+//  }
 }
 
 
