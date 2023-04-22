407f53d371be3f7a20d2f427ccd5e98b14b5a368
rachellomas12 <125256700+rachellomas12@users.noreply.github.com>
2023-04-23 09:27:32 +1000

    Module 3- Fundamentals Q4

---
+++ Module3/FundamentalsQ4/FundamentalsQ4.js
@@ -0,0 +1,19 @@
+// 4. Rewrite this if using the ternary/conditional operator '?'. Test it with different values for a
+// and b. What does the ‘+=’ do?
+
+// let a = 2, b = 3;
+
+// let result = `${a} + ${b} is `;
+// if (a + b < 10) {
+// result += 'less than 10';
+// } else {
+// result += 'greater than 10';
+// }
+
+let a = 4, b = 3;
+let result = (a + b ? 'less than 10' : 'greater than 10');
+console.log(result)
+
+
+
+
