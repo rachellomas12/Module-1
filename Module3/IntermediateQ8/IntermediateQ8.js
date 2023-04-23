f12b86599913568c16fdcef3ae22afafd15b3e5d
rachellomas12 <125256700+rachellomas12@users.noreply.github.com>
2023-04-23 18:53:04 +1000

    Module 3- Intermediate Q8 Fixed

--- Module3/IntermediateQ8/IntermediateQ8.js
+++ Module3/IntermediateQ8/IntermediateQ8.js
@@ -4,29 +4,37 @@ const phoneBookABC = new Map() //an empty map to begin with
 phoneBookABC.set('Annabelle', '0412312343')
 phoneBookABC.set('Barry', '0433221117')
 phoneBookABC.set('Caroline', '0455221182')
-console.log(phoneBookABC);
 
 // a) Create a new phoneBookDEF Map to store names beginning with D, E or F
 
-const phoneBookDEF = new Map() 
-phoneBookABC.set('Darren', '0467854388')
-phoneBookABC.set('Erin', '0439977880')
+const phoneBookDEF = new Map()
+phoneBookABC.set('Darren', '0467854388'),
+phoneBookABC.set('Erin', '0439977880'),
 phoneBookABC.set('Florence', '0432569843')
-console.log(phoneBookDEF);
+console.log(phoneBookDEF)
+
 
 // b) Initialise the contents of phoneBookDEF by passing in an array of keys/values
+const phoneBookDEFArray = new Map ([['Darren', '0467854388'],
+['Erin', '0439977880'],
+['Florence', '0432569843']])
 
 // c) Update the phone number for Caroline
-phoneBookDEF.set('Caroline', '0455221183', [phoneBookDEF.get('Caroline', '0455221183')]);
-console.log(phoneBookDEF.get('Caroline'));
+phoneBookABC.set('Caroline', '0426845697')
 
 // d) Write a function printPhoneBook(contacts) that prints the names and phone
 // numbers in the given Map
-console.log(phoneBookABC);
-console.log(phoneBookDEF);
+function printPhoneBook(contacts){
+    contacts.forEach((value, key) =>{
+        console.log(key + ':' + value)
+    })
+}
 
 // e) Combine the contents of the two individual Maps into a single phoneBook Map
+const phoneBook = new Map([...phoneBookABC, ...phoneBookDEF]);
+
 // f) Print out the full list of names in the combined phone book
+console.log(phoneBook)
 
 
   
