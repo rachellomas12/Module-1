6ec98842914018cddd8b4c6d888121a70a9f001b
rachellomas12 <125256700+rachellomas12@users.noreply.github.com>
2023-04-25 18:43:06 +1000

    Module 3- Fundamentals Q10- Fixed

--- Module3/FundamentalsQ10/FundamentalsQ10.js
+++ Module3/FundamentalsQ10/FundamentalsQ10.js
@@ -2,20 +2,44 @@
 // age values.
 // a) Create a new person using the constructor function and store it in a variable
 
-function Person() { 
-    this.name = 'Rachel' 
-    this.age = '29'
-    this.human = true
+class Person {
+  constructor() {
+    this.name = 'Rachel';
+    this.age = '12';
+    this.human = true;
+  };
 }
+   function canDrive(person) {
+      if (person.age >= 17) {
+        return true;
+      } 
+      else {
+        return false;
+      }
+    }
+    
+  
+  
 const person1 = new Person();
 
 // b)  Create a second person using different name and age values and store it in a separate 
 // variable
-function secondPerson() { 
-    this.name = 'Jimmy'; 
-    this.age = '30'; 
-    this.human = true; 
+class secondPerson {
+  constructor() {
+    this.name = 'Jimmy';
+    this.age = 30;
+    this.human = true;
+  };
 }
+  function canDrive(secondPerson) {
+    if (secondPerson.age >= 16){
+      return true;
+    } 
+    else {
+      return false;
+    }
+  }
+  
 const person2 = new secondPerson();
 
 // c) Print out the properties of each person object to the console
@@ -23,14 +47,20 @@ console.log(person1.name);
 console.log(person2.name);
 console.log(person1.age);
 console.log(person2.age);
+console.log(person1.human);
+console.log(person2.human);
+console.log(canDrive(person1));
+console.log(canDrive(person2));
 
 // d)  Rewrite the constructor function as a class called PersonClass and use it to create a 
 // third person using different name and age values. Print it to the console as well. 
 
-function PersonClass() { 
-    this.name = 'Mary'; 
-    this.age = '31'; 
-    this.human = true; 
+class PersonClass {
+  constructor() {
+    this.name = 'Mary';
+    this.age = '13';
+    this.human = true;
+  }
 }
 const person3 = new PersonClass();
 
@@ -40,21 +70,4 @@ console.log(person3.age);
 // e)  Add a canDrive method to both the constructor function and the class that returns true 
 // if the person is old enough to drive
 
-function canDrive(age) {
-    // your code here
-    if (age >= 17){
-      return false;
-    } else {
-      return false;
-    }
-  }
 
-console.log(person1.name);
-console.log(person2.name);
-console.log(person1.age);
-console.log(person2.age);
-console.log(person3.name);
-console.log(person3.age);
-console.log(person1.human);
-console.log(person2.human);
-console.log(person3.human);
