9aa22a01ce2483a16011f5cc0da737b36d4e206a
rachellomas12 <125256700+rachellomas12@users.noreply.github.com>
2023-04-30 10:07:38 +1000

    Module 3- Advanced Q6

---
+++ Module3/AdvancedQ6/AdvancedQ6.js
@@ -0,0 +1,40 @@
+// 6. Use the Function prototype to add a new delay(ms) function to all functions, which can be used to delay the call to that function by ms milliseconds. 
+// a) Use the example multiply function below to test it with, as above, and assume that all delayed functions will take two parameters
+// b) Use apply to improve your solution so that delayed functions can take any number of parameters
+// c) Modify multiply to take 4 parameters and multiply all of them, and test that your delay prototype function still works.
+
+Function.prototype.delay = function(delay){
+    return (a, b) => {
+        setTimeout(() => { this(a, b)}, delay);
+    }
+}
+function multiply(a, b, c, d) { 
+    console.log( a * b * c * d); 
+}
+function add(a, b) {
+    console.log(a + b);
+}
+function substract(a, b) {
+    console.log(a - b);
+}
+function divide(a, b) {
+    console.log(a / b);
+}
+multiply.delay(500)(5, 5, 5, 5); // prints 25 after 500 milliseconds
+add.delay(500)(5, 5);
+substract.delay(500)(5,5);
+divide.delay(500)(5,5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
